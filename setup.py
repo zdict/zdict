2a5e96c4a3eefad031d198401110a86808744112
Iblis Lin <e196819@hotmail.com>
2015-04-13 01:32:55 +0800

    Introduction to distutils

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='ydict',
+    version='1.3.4',
+    description='yahoo dictionary',
+    author='Shun-Yi Jheng',
+    author_email='shunyi@cs.nctu.edu.tw',
+    maintainer='Iblis Lin',
+    maintainer_email='e196819@hotmail.com',
+    url='https://github.com/M157q/ydict',
+    py_modules=['ydict'],
+)
