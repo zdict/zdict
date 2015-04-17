b3c16c8675047eed98081c25e125e223e6bab7ad
m157q <m157q.tw@gmail.com>
2015-04-17 09:39:30 +0800

    make name of BASE_DIR and DB_FILE more flexible
    prepare for rcfiles.
    if user doesn't define, use the default setting

--- ydict/constants.py
+++ ydict/constants.py
@@ -3,6 +3,8 @@ import os
 
 VERSION = '2.0.0'
 
-BASE_DIR = os.path.join(os.getenv('HOME'), '.ydict')
+BASE_DIR_NAME = '.ydict'
+BASE_DIR = os.path.join(os.getenv('HOME'), BASE_DIR_NAME)
 
-DB_FILE = os.path.join(BASE_DIR, 'db.sqlite')
+DB_NAME = 'sqlite.db'
+DB_FILE = os.path.join(BASE_DIR, DB_NAME)
