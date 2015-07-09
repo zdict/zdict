8f606097c519c15b95ced4dd826ee36ab6217871
shunyi <m157q.tw@gmail.com>
2015-07-09 13:10:11 +0800

    rename sqlite.db to zdict.db

--- zdict/constants.py
+++ zdict/constants.py
@@ -6,5 +6,5 @@ VERSION = '0.0.3'
 BASE_DIR_NAME = '.zdict'
 BASE_DIR = os.path.join(os.getenv('HOME'), BASE_DIR_NAME)
 
-DB_NAME = 'sqlite.db'
+DB_NAME = 'zdict.db'
 DB_FILE = os.path.join(BASE_DIR, DB_NAME)
