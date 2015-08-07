f13f95b55cb3ce8b687a861f7d9f24c7736ab600
許邱翔 <wdv4758h@gmail.com>
2015-08-07 20:37:11 +0800

    add moedict support

---
+++ zdict/plugins/moedict.py
@@ -0,0 +1,84 @@
+import json
+
+from ..dictionaries import DictBase
+from ..exceptions import QueryError, NotFoundError
+from ..models import Record
+
+
+class MoeDict(DictBase):
+
+    API = 'https://www.moedict.tw/uni/{word}'
+
+    @property
+    def provider(self):
+        return 'moe'
+
+    def _get_url(self, word) -> str:
+        return self.API.format(word=word)
+
+    def show(self, record: Record):
+        content = json.loads(record.content)
+
+        # print word
+        self.color.print(content.get('title', ''), 'yellow')
+
+        for word in content.get('heteronyms', ''):
+
+            # print pronounce
+            for key, display in (('bopomofo', '注音'), ('bopomofo2', '注音二式'), ('pinyin', '漢語拼音')):
+                self.color.print(display, end='')
+                self.color.print('[' + word.get(key, '') + ']', 'lwhite', end=' ')
+            print()
+            print()
+
+            # print explain
+            for count, explain in enumerate(word.get('definitions', '')):
+
+                self.color.print(
+                    '{order}. {text}'.format(order=count+1, text=explain.get('def', '')),
+                )
+
+                if explain.get('synonyms'):
+                    self.color.print(
+                        '同義詞: {text}'.format(text=explain['synonyms']),
+                        'magenta',
+                        indent=2,
+                    )
+
+                if explain.get('antonyms'):
+                    self.color.print(
+                        '反義詞: {text}'.format(text=explain['antonyms']),
+                        'magenta',
+                        indent=2,
+                    )
+
+                for example in explain.get('example', ''):
+                    self.color.print(
+                        example,
+                        'indigo',
+                        indent=2,
+                    )
+
+                for quote in explain.get('quote', ''):
+                    self.color.print(
+                        '[引用] {text}'.format(text=quote),
+                        'green',
+                        indent=2,
+                    )
+
+                print()
+
+
+    def query(self, word: str, timeout: float, verbose=False):
+        try:
+            content = self._get_raw(word, timeout)
+        except QueryError as exception:
+            raise NotFoundError(exception.word)
+
+        record = Record(
+                    word=word,
+                    content=content,
+                    source=self.provider,
+                 )
+
+        return record
