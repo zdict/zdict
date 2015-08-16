d437f28f8c3fdf12fdb32f918f4df6f1c28fe6d7
許邱翔 <wdv4758h@gmail.com>
2015-08-16 12:50:41 +0800

    plugin: add Urban dict support

--- zdict/dictionaries/__init__.py
+++ zdict/dictionaries/__init__.py
@@ -1,2 +1,3 @@
 from .yahoo import YahooDict as yahoo
 from .moe import MoeDict as moe
+from .urban import UrbanDict as urban
---
+++ zdict/dictionaries/urban.py
@@ -0,0 +1,50 @@
+import json
+
+from ..dictionary import DictBase
+from ..models import Record
+
+class UrbanDict(DictBase):
+
+    API = 'http://api.urbandictionary.com/v0/define?term={word}'
+
+    @property
+    def provider(self):
+        return 'urban'
+
+    def _get_url(self, word) -> str:
+        return self.API.format(word=word)
+
+    def show(self, record: Record):
+        content = json.loads(record.content)
+
+        data = content['list'][0]
+
+        # print word
+        self.color.print(data.get('word', ''), 'yellow')
+
+        self.color.print(
+            data.get('definition', ''),
+            'org',
+            indent=2,
+        )
+
+        for example in data.get('example', '').split('\n'):
+            self.color.print(
+                example,
+                'indigo',
+                indent=2,
+            )
+
+        print()
+
+
+    def query(self, word: str, timeout: float, verbose=False):
+        content = self._get_raw(word, timeout)
+
+        record = Record(
+                    word=word,
+                    content=content,
+                    source=self.provider,
+                 )
+
+        return record
