5d887936e61d80016c08709aff36f86bb88063f0
shunyi <M157q.tw@gmail.com>
2015-09-05 03:34:26 +0800

    ref #28 - Add template.py into zdict/dictionaries/
    Need to add README for the details about how to contribute new dictionary
    and some API documentation for the future.

---
+++ zdict/dictionaries/template.py
@@ -0,0 +1,70 @@
+import json
+
+from bs4 import BeautifulSoup
+
+from ..dictionary import DictBase
+from ..exceptions import NotFoundError
+from ..models import Record
+
+
+# Don't forget to register the new dictionary at `zdict/dictionaries/__init__.py`
+
+# Change `Template` to the name of new dictionary. like xxxDict.
+class TemplateDict(DictBase):
+
+    # Change the url below to the API url of the new dictionary.
+    API = 'https://tw.dictionary.search.yahoo.com/search?p={word}'
+    # Need to keep the `{word}` for `_get_url()` usage.
+
+    @property
+    def provider(self):
+        # Change `template` to the short name of the new dictionary.
+        return 'template'
+
+
+    def _get_url(self, word) -> str:
+        return self.API.format(word=word)
+
+
+    def show(self, record: Record, verbose=False):
+        content = json.loads(record.content)
+
+        # Use `self.color.print()` to render the output.
+        # TODO: Add the API doc of the `color.print()`
+        print()
+
+        if verbose:
+            try:
+                # Get the addtional information if it exists.
+            except:
+                return
+            else:
+                # Define how to print the additional information
+                print()
+
+
+    def query(self, word: str, timeout: float, verbose=False):
+        webpage = self._get_raw(word, timeout)
+        data = BeautifulSoup(webpage, "html.parser")
+        content = {}
+
+        # Parse `data` and fill the information you need into `content`
+        # Use
+        # ```
+        # except AttributeError:
+        #    raise NotFoundError(word)
+        # ```
+        # while the word users try to query is not found on this dictionary.
+
+        if verbose:
+            # Store the additonal information for the verbose option.
+
+        record = Record(
+                    word=word,
+                    content=json.dumps(content),
+                    source=self.provider,
+                 )
+
+        return record
+
+# Don't forget to register the new dictionary at `zdict/dictionaries/__init__.py`
