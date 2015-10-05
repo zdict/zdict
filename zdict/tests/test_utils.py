4d1bf03b8fb61a18b353e80b7f0956057cc3f09d
Iblis Lin <e196819@hotmail.com>
2015-10-05 20:36:49 +0800

    [#42] Add test cases
    1. create_zdict_db_if_not_exists
    2. create_zdict_dir_if_not_exists

--- zdict/tests/test_utils.py
+++ zdict/tests/test_utils.py
@@ -1,5 +1,7 @@
-from ..utils import Color
+from ..utils import (Color, create_zdict_db_if_not_exists,
+                     create_zdict_dir_if_not_exists)
 from pytest import raises
+from unittest.mock import patch
 
 
 class TestColor:
@@ -19,3 +21,25 @@ class TestColor:
         self.color.lred = '\33[31;1m'
         with raises(AttributeError):
             self.color.test
+
+
+@patch('zdict.utils.constants')
+@patch('os.mkdir')
+@patch('os.path.isdir', return_value=False)
+def test_create_zdict_dir_if_not_exists(mkdir, isdir, constants):
+    constants.BASE_DIR = '/mock'
+    create_zdict_dir_if_not_exists()
+
+    isdir.assert_called_with('/mock')
+    mkdir.assert_called_with('/mock')
+
+
+@patch('builtins.open')
+@patch('zdict.utils.constants')
+@patch('os.path.exists', return_value=False)
+def test_create_zdict_db_if_not_exists(exists, constants, open):
+    constants.DB_FILE = '/mock'
+    create_zdict_db_if_not_exists()
+
+    exists.assert_called_with('/mock')
+    assert open.called
