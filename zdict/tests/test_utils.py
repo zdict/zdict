449a7f043254b425ac4bdcaa93d44bc434e5487b
Iblis Lin <e196819@hotmail.com>
2015-10-05 18:23:45 +0800

    [#40] Migrate test_utils.py

--- zdict/tests/test_utils.py
+++ zdict/tests/test_utils.py
@@ -1,28 +1,21 @@
-import unittest
-
 from ..utils import Color
+from pytest import raises
 
 
-class TestColor(unittest.TestCase):
-    def setUp(self):
+class TestColor:
+    def setup_method(self, method):
         self.color = Color()
 
+    def teardown_method(self, method):
+        del self.color
+
     def test_format(self):
-        self.assertEqual(
-            '\33[31;1mtest\33[0m',
-            self.color.format('test', 'lred')
-        )
-        self.assertEqual(
-            '\33[31mtest\33[0m',
-            self.color.format('test', 'red')
-        )
-        self.assertEqual(
-            '  \33[31mtest\33[0m',
-            self.color.format('test', 'red', indent=2)
-        )
+        assert '\33[31;1mtest\33[0m', self.color.format('test', 'lred')
+        assert '\33[31mtest\33[0m', self.color.format('test', 'red')
+        assert '  \33[31mtest\33[0m', self.color.format('test', 'red', indent=2)
 
     def test_attribute(self):
         self.color.red = '\33[31m'
         self.color.lred = '\33[31;1m'
-        with self.assertRaises(AttributeError):
+        with raises(AttributeError):
             self.color.test
