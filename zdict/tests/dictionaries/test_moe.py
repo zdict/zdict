c5ef1ab283c7c7589b5718ef019dacc9ac602262
Iblis Lin <e196819@hotmail.com>
2015-10-05 18:25:58 +0800

    [#40] Migrate test_moe.py

--- zdict/tests/dictionaries/test_moe.py
+++ zdict/tests/dictionaries/test_moe.py
@@ -1,14 +1,12 @@
-import unittest
-
 from ...dictionaries import moe
 
 
-class TestyDict(unittest.TestCase):
-    def setUp(self):
+class TestyDict:
+    def setup_method(self, method):
         self.dict = moe()
 
+    def teardown_method(self, method):
+        del self.dict
+
     def test__get_url(self):
-        self.assertEqual(
-            'https://www.moedict.tw/uni/萌',
-            self.dict._get_url('萌')
-        )
+        assert 'https://www.moedict.tw/uni/萌', self.dict._get_url('萌')
