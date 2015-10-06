f9ecd13e64bfce12b7c6b6540548ddc3ea8a8c8b
Iblis Lin <e196819@hotmail.com>
2015-10-06 21:38:02 +0800

    [#43] Test cases for MoeDict.query

--- zdict/tests/dictionaries/test_moe.py
+++ zdict/tests/dictionaries/test_moe.py
@@ -1,4 +1,8 @@
 from ...dictionaries.moe import MoeDict
+from ...exceptions import NotFoundError, QueryError
+
+from pytest import raises
+from unittest.mock import Mock, patch
 
 
 class TestyDict:
@@ -13,3 +17,19 @@ class TestyDict:
 
     def test_provider(self):
         assert self.dict.provider == 'moe'
+
+    def test_query_timeout(self):
+        self.dict._get_raw = Mock(side_effect=QueryError('萌', 1))
+
+        with raises(NotFoundError):
+            self.dict.query('萌', timeout=666)
+
+        self.dict._get_raw.assert_called_with('萌', 666)
+
+    @patch('zdict.dictionaries.moe.Record')
+    def test_query_normal(self, Record):
+        self.dict._get_raw = Mock(return_value='{}')
+
+        record = self.dict.query('萌', timeout=666)
+
+        Record.assert_called_with(word='萌', content='{}', source='moe')
