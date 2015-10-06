9422f1d9cc7142e7c6c7d2fb1f015abd5cf58b8d
Iblis Lin <e196819@hotmail.com>
2015-10-06 21:17:16 +0800

    [#44] Add test cases for loader.py

---
+++ zdict/tests/test_loader.py
@@ -0,0 +1,50 @@
+from ..loader import _is_dict, get_dictionary_map
+from ..dictionary import DictBase
+
+from unittest.mock import MagicMock, Mock, patch
+
+
+@patch('builtins.issubclass', return_value=True)
+def test__is_dict_normal(issubclass_):
+    assert _is_dict(Mock) is True
+    issubclass_.assert_called_with(Mock, DictBase)
+
+
+@patch('builtins.issubclass', return_value=True)
+def test__is_dict_DictBase(issubclass_):
+    assert _is_dict(DictBase) is False
+    issubclass_.assert_called_with(DictBase, DictBase)
+
+
+@patch('builtins.issubclass', side_effect=TypeError)
+def test__is_dict_not_class(issubclass_):
+    import sys
+
+    assert _is_dict(sys) is False
+    issubclass_.called
+
+
+@patch('zdict.loader.getmembers')
+@patch('zdict.loader.import_module', return_value='mock')
+@patch('zdict.loader.os.listdir')
+def test_get_dictionary_map(listdir, import_module, getmembers):
+    # prepare mocked return value
+    MockDict = MagicMock(name='MockDict')
+    MockDict().provider = 'mock'
+    listdir.return_value = [
+        '__init__.py',
+        '_strange.py',
+        'mock.py',
+        'template.py',  # the excluded files
+        'not_a_py.rst',
+    ]
+    getmembers.return_value = [('mock', MockDict)]
+    
+    dict_map = get_dictionary_map()
+
+    assert dict_map == {'mock': MockDict}
+    assert listdir.called
+    import_module.assert_called_with('zdict.dictionaries.mock')
+    assert import_module.call_count == 1
+    getmembers.assert_called_with('mock', predicate=_is_dict)
+    assert MockDict.called
