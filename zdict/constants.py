22e1aff4a083bb1c97680802d22ffc790e896f19
shunyi <M157q.tw@gmail.com>
2017-12-07 17:21:58 +0800

    [#148] Fix the bug when getting user home dir on Windows
    Use `os.path.expanduser` instead of using `os.path.join` directly,
    since the formmer is better handled for Windows.
    
    ref: <https://docs.python.org/3/library/os.path.html#os.path.expanduser>

--- zdict/constants.py
+++ zdict/constants.py
@@ -4,7 +4,7 @@ import os
 VERSION = '0.10.1'
 
 BASE_DIR_NAME = '.zdict'
-BASE_DIR = os.path.join(os.getenv('HOME'), BASE_DIR_NAME)
+BASE_DIR = os.path.join(os.path.expanduser("~"), BASE_DIR_NAME)
 
 DB_NAME = 'zdict.db'
 DB_FILE = os.path.join(BASE_DIR, DB_NAME)
