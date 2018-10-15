344a003169ac6b83f7c73cfceec23acc1ee43835
Aquakor <kornelux1@gmail.com>
2018-10-15 18:33:29 +0200

    Add test for Wiktionary

---
+++ zdict/tests/dictionaries/test_wiktionary.py
@@ -0,0 +1,68 @@
+from pytest import raises
+from unittest.mock import Mock, patch
+
+from zdict.dictionaries.wiktionary import WiktionaryDict
+from zdict.exceptions import NotFoundError, QueryError
+from zdict.models import Record
+from zdict.zdict import get_args
+
+
+class TestWiktionaryDict:
+    def setup_method(self, method):
+        self.dict = WiktionaryDict(get_args())
+
+    def teardown_method(self, method):
+        del self.dict
+
+    def test_provider(self):
+        assert self.dict.provider == 'wiktionary'
+
+    def test__get_url(self):
+        uri = 'https://en.wiktionary.org/api/rest_v1/page/definition/mock'
+        assert self.dict._get_url('mock') == uri
+
+    def test_query_notfound(self):
+        self.dict._get_raw = Mock(side_effect=QueryError('mock', 404))
+
+        with raises(NotFoundError):
+            self.dict.query('mock')
+
+        self.dict._get_raw.assert_called_with('mock')
+
+    @patch('zdict.dictionaries.wiktionary.Record')
+    def test_query_normal(self, Record):
+        self.dict._get_raw = Mock(return_value='{"mock": true}')
+        self.dict.query('mock')
+        Record.assert_called_with(
+            word='mock',
+            content='{"mock": true}',
+            source='wiktionary'
+        )
+
+    def test_show(self):
+        content = '''
+        {
+           "en":[
+              {
+                 "partOfSpeech":"string",
+                 "language":"string",
+                 "definitions":[
+                    {
+                       "definition":"string",
+                       "parsedExamples":[
+                          {
+                             "example":"string"
+                          }
+                       ],
+                       "examples":[
+                          "string"
+                       ]
+                    }]
+                }]
+        }
+        '''
+
+        r = Record(word='string',
+                   content=content,
+                   source=self.dict.provider)
+        self.dict.show(r)
