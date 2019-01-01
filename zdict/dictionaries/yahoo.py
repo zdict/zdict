d6c9eb5842414aed469a8fde9111949fdf07d30b
shunyi <M157q.tw@gmail.com>
2019-01-01 21:50:03 +0800

    [#296] Fix NotFoundError of YahooDict
    if node is None => should return NotFoundError

--- zdict/dictionaries/yahoo.py
+++ zdict/dictionaries/yahoo.py
@@ -234,18 +234,17 @@ class YahooDict(DictBase):
             return list(map(text, d.select(s)))
 
         node = data.select_one('div#web ol.searchCenterMiddle > li > div')
-
         p = None  # optional
-        if len(node) == 6:    # e.g. "metadata"
-            _, w, p, _, _, e = node
-        elif len(node) == 5:
-            _, w, p, _, e = node
-        elif len(node) == 4:  # e.g. "hold on"
-            _, w, _, e = node
+        if node is None or len(node) <= 2:  # e.g. "fabor"
+            raise NotFoundError(word)
         elif len(node) == 3:  # e.g. "google"
             _, w, e = node
-        elif len(node) <= 2:  # e.g. "fabor"
-            raise NotFoundError(word)
+        elif len(node) == 4:  # e.g. "hold on"
+            _, w, _, e = node
+        elif len(node) == 5:
+            _, w, p, _, e = node
+        elif len(node) == 6:    # e.g. "metadata"
+            _, w, p, _, _, e = node
 
         return {
             'word': w.find('span').text.strip(),
