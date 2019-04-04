946500cb27ef3601f9d0d41eea8d2ceab1510641
shunyi <M157q.tw@gmail.com>
2019-04-04 18:16:20 +0800

    [CI] Travis CI: Remove openssl@1.1 for homebrew
    Make it use openssl only.
    
    The root cause is this PR of pyenv merged 3 days ago:
    <https://github.com/pyenv/pyenv/pull/1302>
    
    It didn't handle properly when user installed openssl and openssl@1.1
    both via homebrew.
    
    Which makes user encounter these errors:
    ```
    *** WARNING: renaming "_ssl" since importing it failed:
    dlopen(build/lib.macosx-10.13-x86_64-3.4/_ssl.so, 2): Symbol not found:
    _CRYPTO_THREADID_set_callback
    ```
    ```
    Failed to build these modules:
    _hashlib              _ssl
    ```
    
    Because `_ssl` has been renamed, it can not been found while installing
    pip, which makes the build fail.

--- ci/install_python_for_osx.sh
+++ ci/install_python_for_osx.sh
@@ -4,13 +4,13 @@ set -ev
 
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
+# Make it use openssl only
+brew uninstall openssl@1.1
+
 # print openssl path
-brew --prefix openssl@1.1
+brew --prefix openssl
 
 # Install specific version of Python via pyenv
-LDFLAGS="-L$(brew --prefix openssl@1.1)/lib" \
-CPPFLAGS="-I$(brew --prefix openssl@1.1)/include" \
-CFLAGS="-I$(brew --prefix openssl@1.1)/include" \
 ~/.pyenv/bin/pyenv install -v $PYTHON_VERSION
 
 # Create venv with the specific Python version
