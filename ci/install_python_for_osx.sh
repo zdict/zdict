ef0f93fc874af9c1446cd8628e6e5d64bdb4a0de
shunyi <M157q.tw@gmail.com>
2019-04-04 18:01:00 +0800

    [CI] Travis CI: Fix pyenv compilation for pyenv 1.2.10

--- ci/install_python_for_osx.sh
+++ ci/install_python_for_osx.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/sh
 
+set -ev
+
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-~/.pyenv/bin/pyenv install $PYTHON_VERSION
-~/.pyenv/versions/$PYTHON_VERSION/bin/python3 --version
+
+# print openssl path
+brew --prefix openssl@1.1
+
+# Install specific version of Python via pyenv
+LDFLAGS="-L$(brew --prefix openssl@1.1)/lib" \
+CPPFLAGS="-I$(brew --prefix openssl@1.1)/include" \
+CFLAGS="-I$(brew --prefix openssl@1.1)/include" \
+~/.pyenv/bin/pyenv install -v $PYTHON_VERSION
+
+# Create venv with the specific Python version
 ~/.pyenv/versions/$PYTHON_VERSION/bin/python3 -m venv ~/venv
